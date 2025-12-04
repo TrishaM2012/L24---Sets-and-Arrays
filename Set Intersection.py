7f77054d6ea8e7cce1c8c44861ae31f80ec44ede
TrishaM2012 <Sandy.avaya@gmail.com>
2025-12-04 17:03:42 +0000

    initial commit

---
+++ Set Intersection.py
@@ -0,0 +1,10 @@
+setx = {"green" , "blue"}
+sety = {"blue" , "yellow"}
+print(setx)
+print(sety)
+print("\n Intersection of the two sets: ")
+setz = setx.intersection(sety)
+print(setz)
+print("\n The Union of the two sets: ")
+setz = setx.union(sety)
+print(setz)
